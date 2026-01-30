63b55d4487f3223d044620488f9d6e89da5bc3af
andreagarme <andreagarme@gmail.com>
2026-01-30 01:15:13 +0000

    Crear Busquedas Previas

--- gifsApp/src/GifsApp.tsx
+++ gifsApp/src/GifsApp.tsx
@@ -13,8 +13,11 @@ export const GifsApp = () => {
     console.log(`Termino de busqueda seleccionado: ${term}`);
   }
 
-  const handleSearch = (query: string) => {
-      console.log(query);  
+  const handleSearch = (query: string = '') => {
+      query = query.trim().toLowerCase();
+      if (query.length === 0) return;
+      if (previousTerms.includes(query)) return; 
+     setPreviousTerms([query, ...previousTerms].slice(0,8));
   };
 
   return (   
